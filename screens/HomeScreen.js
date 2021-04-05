efbcf48f7a1c032cd1c03b88261061227a400dcf
Alien178 <57078903+Alien178@users.noreply.github.com>
2021-04-04 20:04:16 -0700

    Update HomeScreen.js

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -25,7 +25,7 @@ export default class HomeScreen extends React.Component {
 
   fetchData = () => {
     fetch(
-      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.recentSearch}&type=video&key=AIzaSyCUBqJUmKcL6M0cS7KDDQ_KM0ooTjWwz88`
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${this.state.recentSearch}&type=video&key=AIzaSyCUBqJUmKcL6M0cS7KDDQ_KM0ooTj`
     )
       .then((res) => res.json())
       .then((data) => {
